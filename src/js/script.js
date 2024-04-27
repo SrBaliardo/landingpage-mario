00418cca5758fb0903823a9a318b65d86c8b68fb
SrBaliardo <tao.john007@icloud.com>
2024-04-27 17:47:24 -0300

    First commit - v1.0

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('.form-contact')
+const formBg = document.querySelector('.form-bg')
+
+function viewForm() {
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    formBg.style.visibility = "visible"
+}
+
+function hiddenForm() {
+    form.style.left = "-300px"
+    formBg.style.visibility = "hidden"
+}
