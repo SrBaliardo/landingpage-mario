425bd68afc31b1c82edeac1bd2c86b23551718a3
SrBaliardo <tao.john007@icloud.com>
2025-01-23 09:12:18 -0300

    fix(code): README and some code lines.

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,13 @@
-const form = document.querySelector('.form-contact')
-const formBg = document.querySelector('.form-bg')
+const form = document.querySelector(".form-contact");
+const formBg = document.querySelector(".form-bg");
 
 function viewForm() {
-    form.style.left = "50%"
-    form.style.transform = "translateX(-50%)"
-    formBg.style.visibility = "visible"
+  form.style.left = "50%";
+  form.style.transform = "translateX(-50%)";
+  formBg.style.visibility = "visible";
 }
 
 function hiddenForm() {
-    form.style.left = "-300px"
-    formBg.style.visibility = "hidden"
+  form.style.left = "-300px";
+  formBg.style.visibility = "hidden";
 }
